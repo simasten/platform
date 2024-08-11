188c6fe513b21ea6ee562b35a797516d2c5df68f
monoland <mono.socialmedia@gmail.com>
2024-08-11 22:03:10 +0700

    update

--- frontend/src/components/desktop/FileList/index.vue
+++ frontend/src/components/desktop/FileList/index.vue
@@ -1,39 +1,50 @@
 <template>
-    <v-sheet>
-        <slot></slot>
+	<v-sheet>
+		<slot></slot>
 
-        <v-dialog
-            v-model="dialogFile"
-            transition="dialog-bottom-transition"
-            fullscreen
-        >
-            <v-sheet height="100vh">
-                <v-toolbar :color="theme">
-                    <v-btn icon @click="dialogFile = false">
-                        <v-icon>close</v-icon>
-                    </v-btn>
+		<v-dialog
+			v-model="dialogFile"
+			transition="dialog-bottom-transition"
+			fullscreen
+		>
+			<v-sheet height="100vh">
+				<v-toolbar :color="theme">
+					<v-btn
+						icon
+						@click="dialogFile = false"
+					>
+						<v-icon>close</v-icon>
+					</v-btn>
 
-                    <v-toolbar-title class="text-overline"
-                        >document</v-toolbar-title
-                    >
+					<v-toolbar-title class="text-overline"
+						>document</v-toolbar-title
+					>
 
-                    <v-btn size="small" icon @click="$refs.pdf.zoomIn()">
-                        <v-icon size="x-large">add_circle</v-icon>
-                    </v-btn>
+					<v-btn
+						size="small"
+						icon
+						@click="$refs.pdf.zoomIn()"
+					>
+						<v-icon size="x-large">add_circle</v-icon>
+					</v-btn>
 
-                    <v-btn size="small" icon @click="$refs.pdf.zoomOut()">
-                        <v-icon size="x-large">remove_circle</v-icon>
-                    </v-btn>
-                </v-toolbar>
+					<v-btn
+						size="small"
+						icon
+						@click="$refs.pdf.zoomOut()"
+					>
+						<v-icon size="x-large">remove_circle</v-icon>
+					</v-btn>
+				</v-toolbar>
 
-                <view-pdf
-                    ref="pdf"
-                    :datapath="datapath"
-                    :value="currentFile"
-                ></view-pdf>
-            </v-sheet>
-        </v-dialog>
-    </v-sheet>
+				<widget-pdf
+					ref="pdf"
+					:datapath="datapath"
+					:value="currentFile"
+				></widget-pdf>
+			</v-sheet>
+		</v-dialog>
+	</v-sheet>
 </template>
 
 <script>
@@ -41,22 +52,22 @@ import { usePageStore } from "@pinia/pageStore";
 import { storeToRefs } from "pinia";
 
 export default {
-    name: "file-list",
+	name: "file-list",
 
-    props: {
-        datapath: String,
-    },
+	props: {
+		datapath: String,
+	},
 
-    setup() {
-        const store = usePageStore();
+	setup() {
+		const store = usePageStore();
 
-        const { currentFile, dialogFile, theme } = storeToRefs(store);
+		const { currentFile, dialogFile, theme } = storeToRefs(store);
 
-        return {
-            currentFile,
-            dialogFile,
-            theme,
-        };
-    },
+		return {
+			currentFile,
+			dialogFile,
+			theme,
+		};
+	},
 };
 </script>
