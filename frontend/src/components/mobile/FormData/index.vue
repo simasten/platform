d083017462b594bd792a7445c68cf1bf0c8e524a
monoland <mono.socialmedia@gmail.com>
2024-08-26 17:19:50 +0700

    update

--- frontend/src/components/mobile/FormData/index.vue
+++ frontend/src/components/mobile/FormData/index.vue
@@ -1,187 +1,199 @@
 <template>
-    <v-app-bar
-        :color="`${theme}`"
-        scroll-behavior="hide elevate"
-        scroll-threshold="64"
-    >
-        <v-btn
-            icon
-            v-if="parentName"
-            @click="
-                manualBacknav
-                    ? $emit('click:backnav', $event)
-                    : $router.push({ name: parentName })
-            "
-        >
-            <v-icon>arrow_back</v-icon>
-        </v-btn>
+	<v-app-bar
+		:color="`${theme}`"
+		scroll-behavior="hide elevate"
+		scroll-threshold="64"
+	>
+		<v-btn
+			icon
+			v-if="parentName"
+			@click="
+				manualBacknav
+					? $emit('click:backnav', $event)
+					: $router.push({ name: parentName })
+			"
+		>
+			<v-icon>arrow_back</v-icon>
+		</v-btn>
 
-        <v-toolbar-title class="text-body-2 font-weight-bold text-uppercase">{{
-            page.name ?? module.name
-        }}</v-toolbar-title>
+		<v-toolbar-title class="text-body-2 font-weight-bold text-uppercase">{{
+			page.name ?? module.name
+		}}</v-toolbar-title>
 
-        <v-spacer></v-spacer>
+		<v-spacer></v-spacer>
 
-        <v-btn
-            v-if="!disableCreate && !hasSelected"
-            :color="highlight"
-            icon
-            @click="openFormCreate"
-        >
-            <v-icon class="with-shadow">add</v-icon>
-        </v-btn>
+		<v-btn
+			v-if="!disableCreate && !hasSelected"
+			:color="highlight"
+			icon
+			@click="openFormCreate"
+		>
+			<v-icon class="with-shadow">add</v-icon>
+		</v-btn>
 
-        <v-btn
-            :disabled="hasSelected"
-            :color="sidenavState ? 'white' : `${theme}-lighten-3`"
-            icon
-            @click="sidenavState = !sidenavState"
-        >
-            <v-icon class="with-shadow">filter_list</v-icon>
-        </v-btn>
-    </v-app-bar>
+		<v-btn
+			:disabled="hasSelected"
+			:color="sidenavState ? 'white' : `${theme}-lighten-3`"
+			icon
+			@click="sidenavState = !sidenavState"
+		>
+			<v-icon class="with-shadow">filter_list</v-icon>
+		</v-btn>
+	</v-app-bar>
 
-    <v-sheet
-        :color="`${theme}`"
-        class="mx-auto position-fixed w-100 rounded-b-xl"
-        height="256"
-    ></v-sheet>
+	<v-sheet
+		:color="`${theme}`"
+		class="mx-auto position-fixed w-100 rounded-b-xl"
+		height="256"
+	></v-sheet>
 
-    <v-main>
-        <v-sheet class="bg-transparent position-relative px-4 pt-9 pb-4">
-            <v-sheet
-                class="position-absolute"
-                color="transparent"
-                width="calc(100% - 32px)"
-                style="top: 0; z-index: 1"
-            >
-                <div class="d-flex justify-center">
-                    <form-icon></form-icon>
+	<v-main>
+		<v-sheet class="bg-transparent position-relative px-4 pt-9 pb-4">
+			<v-sheet
+				class="position-absolute"
+				color="transparent"
+				width="calc(100% - 32px)"
+				style="top: 0; z-index: 1"
+			>
+				<div class="d-flex justify-center">
+					<form-icon></form-icon>
 
-                    <div
-                        :class="`text-${theme}-lighten-4`"
-                        class="text-caption text-white position-absolute font-weight-bold text-uppercase text-left"
-                        style="
-                            top: 8px;
-                            left: 0;
-                            font-size: 0.63rem !important;
-                            width: calc(50% - 30px);
-                        "
-                    >
-                        <div
-                            class="d-inline-block text-truncate"
-                            style="max-width: 100%"
-                        >
-                            {{ title }}
-                        </div>
-                    </div>
+					<div
+						:class="`text-${theme}-lighten-4`"
+						class="text-caption text-white position-absolute font-weight-bold text-uppercase text-left"
+						style="
+							top: 8px;
+							left: 0;
+							font-size: 0.63rem !important;
+							width: calc(50% - 30px);
+						"
+					>
+						<div
+							class="d-inline-block text-truncate"
+							style="max-width: 100%"
+						>
+							{{ title }}
+						</div>
+					</div>
 
-                    <div
-                        :class="`text-${theme}-lighten-4`"
-                        class="text-caption text-white position-absolute font-weight-bold text-uppercase text-right"
-                        style="
-                            font-size: 0.63rem !important;
-                            top: 8px;
-                            right: 0;
-                            width: calc(50% - 30px);
-                        "
-                    >
-                        <div
-                            class="d-inline-block text-truncate"
-                            style="max-width: 100%"
-                        >
-                            SEMUA DATA
-                        </div>
-                    </div>
-                </div>
-            </v-sheet>
+					<div
+						:class="`text-${theme}-lighten-4`"
+						class="text-caption text-white position-absolute font-weight-bold text-uppercase text-right"
+						style="
+							font-size: 0.63rem !important;
+							top: 8px;
+							right: 0;
+							width: calc(50% - 30px);
+						"
+					>
+						<div
+							class="d-inline-block text-truncate"
+							style="max-width: 100%"
+						>
+							SEMUA DATA
+						</div>
+					</div>
+				</div>
+			</v-sheet>
 
-            <v-sheet
-                class="position-relative pt-7"
-                elevation="1"
-                min-height="calc(100dvh - 172px)"
-                rounded="lg"
-                flat
-            >
-                <v-list
-                    class="bg-transparent"
-                    :active-class="
-                        showDelete
-                            ? `bg-white elevation-6 text-grey with-delete`
-                            : `bg-white elevation-6 text-grey`
-                    "
-                    lines="two"
-                    selectable
-                    @update:selected="setSelected"
-                >
-                    <template v-for="(record, index) in records">
-                        <slot
-                            name="mobile"
-                            :index="index"
-                            :record="record"
-                            :showDelete="showDelete"
-                            :theme="theme"
-                        >
-                            <item-data
-                                :chip="chip"
-                                :subtitle="subtitle"
-                                :show-delete="showDelete"
-                                :value="record"
-                            ></item-data>
-                        </slot>
-                    </template>
+			<v-sheet
+				class="position-relative pt-7"
+				elevation="1"
+				min-height="calc(100dvh - 172px)"
+				rounded="lg"
+				flat
+			>
+				<v-list
+					class="bg-transparent"
+					:active-class="
+						showDelete
+							? `bg-white elevation-6 text-grey with-delete`
+							: `bg-white elevation-6 text-grey`
+					"
+					lines="two"
+					selectable
+					@update:selected="setSelected"
+				>
+					<template v-for="(record, index) in records">
+						<slot
+							name="mobile"
+							:index="index"
+							:record="record"
+							:showDelete="showDelete"
+							:theme="theme"
+						>
+							<item-data
+								:chip="chip"
+								:subtitle="subtitle"
+								:show-delete="showDelete"
+								:value="record"
+							></item-data>
+						</slot>
+					</template>
 
-                    <template v-if="records.length <= 0">
-                        <slot>
-                            <div
-                                class="d-flex align-center justify-center text-body-2 text-center text-grey"
-                                style="height: calc(100dvh - 216px)"
-                            >
-                                Data tidak ditemukan
-                            </div>
-                        </slot>
-                    </template>
+					<template v-if="records.length <= 0">
+						<slot>
+							<div
+								class="d-flex align-center justify-center text-body-2 text-center text-grey"
+								style="height: calc(100dvh - 216px)"
+							>
+								Data tidak ditemukan
+							</div>
+						</slot>
+					</template>
 
-                    <template v-else>
-                        <div
-                            v-if="
-                                meta.current_page &&
-                                meta.current_page < meta.last_page
-                            "
-                            class="d-flex align-center justify-center py-2"
-                            style="height: 0px; width: 100%"
-                            v-intersect="onIntersect"
-                        ></div>
-                    </template>
-                </v-list>
-            </v-sheet>
-        </v-sheet>
-    </v-main>
+					<template v-else>
+						<div
+							v-if="
+								meta.current_page &&
+								meta.current_page < meta.last_page
+							"
+							class="d-flex align-center justify-center py-2"
+							style="height: 0px; width: 100%"
+							v-intersect="onIntersect"
+						></div>
+					</template>
+				</v-list>
+			</v-sheet>
+		</v-sheet>
+	</v-main>
 
-    <page-filter :withSync="withSync">
-        <template v-slot:syncdata="{ mapResponseData, parent }">
-            <slot
-                name="syncdata"
-                :mapResponseData="mapResponseData"
-                :params="params"
-                :parent="parent"
-                :store="store"
-                :theme="theme"
-            ></slot>
-        </template>
+	<page-filter :withSync="withSync">
+		<template v-slot:syncdata="{ mapResponseData, parent }">
+			<slot
+				name="syncdata"
+				:mapResponseData="mapResponseData"
+				:params="params"
+				:parent="parent"
+				:store="store"
+				:theme="theme"
+			></slot>
+		</template>
 
-        <template v-slot:forminfo>
-            <slot name="forminfo" :store="store" :theme="theme"></slot>
-        </template>
+		<template v-slot:forminfo>
+			<slot
+				name="forminfo"
+				:store="store"
+				:theme="theme"
+			></slot>
+		</template>
 
-        <template v-slot:helpdesk>
-            <slot name="helpdesk" :store="store" :theme="theme"></slot>
-        </template>
+		<template v-slot:helpdesk>
+			<slot
+				name="helpdesk"
+				:store="store"
+				:theme="theme"
+			></slot>
+		</template>
 
-        <template v-slot:utility>
-            <slot name="utility" :store="store" :theme="theme"></slot>
-        </template>
-    </page-filter>
+		<template v-slot:utility>
+			<slot
+				name="utility"
+				:store="store"
+				:theme="theme"
+			></slot>
+		</template>
+	</page-filter>
 </template>
 
 <script>
@@ -189,114 +201,114 @@ import { usePageStore } from "@pinia/pageStore";
 import { storeToRefs } from "pinia";
 
 export default {
-    name: "form-data",
+	name: "form-data",
 
-    props: {
-        chip: {
-            type: String,
-            default: "chip",
-        },
+	props: {
+		chip: {
+			type: String,
+			default: "chip",
+		},
 
-        disableCreate: Boolean,
-        manualBacknav: Boolean,
+		disableCreate: Boolean,
+		manualBacknav: Boolean,
 
-        subtitle: {
-            type: String,
-            default: "subtitle",
-        },
+		subtitle: {
+			type: String,
+			default: "subtitle",
+		},
 
-        showDelete: {
-            type: Boolean,
-            default: false,
-        },
+		showDelete: {
+			type: Boolean,
+			default: false,
+		},
 
-        withSync: Boolean,
-    },
+		withSync: Boolean,
+	},
 
-    emits: {
-        "click:backnav": null,
-    },
+	emits: {
+		"click:backnav": null,
+	},
 
-    setup() {
-        const store = usePageStore();
+	setup() {
+		const store = usePageStore();
 
-        store.helpState = false;
+		store.helpState = false;
 
-        const {
-            formStateLast,
-            hasSelected,
-            headers,
-            highlight,
-            itemsPerPage,
-            loading,
-            meta,
-            module,
-            navigationState,
-            page,
-            pageKey,
-            params,
-            parentName,
-            records,
-            railMode,
-            sidenavState,
-            selected,
-            title,
-            theme,
-            totalRecords,
-        } = storeToRefs(store);
+		const {
+			formStateLast,
+			hasSelected,
+			headers,
+			highlight,
+			itemsPerPage,
+			loading,
+			meta,
+			module,
+			navigationState,
+			page,
+			pageKey,
+			params,
+			parentName,
+			records,
+			railMode,
+			sidenavState,
+			selected,
+			title,
+			theme,
+			totalRecords,
+		} = storeToRefs(store);
 
-        const { getPageDatas, openFormCreate, openFormShow, setSelected } =
-            store;
+		const { getPageDatas, openFormCreate, openFormShow, setSelected } =
+			store;
 
-        return {
-            formStateLast,
-            hasSelected,
-            headers,
-            highlight,
-            itemsPerPage,
-            loading,
-            meta,
-            module,
-            navigationState,
-            page,
-            pageKey,
-            params,
-            parentName,
-            records,
-            railMode,
-            sidenavState,
-            selected,
-            title,
-            theme,
-            totalRecords,
+		return {
+			formStateLast,
+			hasSelected,
+			headers,
+			highlight,
+			itemsPerPage,
+			loading,
+			meta,
+			module,
+			navigationState,
+			page,
+			pageKey,
+			params,
+			parentName,
+			records,
+			railMode,
+			sidenavState,
+			selected,
+			title,
+			theme,
+			totalRecords,
 
-            getPageDatas,
-            openFormCreate,
-            openFormShow,
-            setSelected,
+			getPageDatas,
+			openFormCreate,
+			openFormShow,
+			setSelected,
 
-            store,
-        };
-    },
+			store,
+		};
+	},
 
-    methods: {
-        onIntersect: function (isIntersecting) {
-            if (isIntersecting) {
-                this.loading = true;
-                this.params.page = this.params.page + 1;
-            }
-        },
-    },
+	methods: {
+		onIntersect: function (isIntersecting) {
+			if (isIntersecting) {
+				this.loading = true;
+				this.params.page = this.params.page + 1;
+			}
+		},
+	},
 
-    watch: {
-        params: {
-            handler: function (newOptions) {
-                this.getPageDatas(newOptions);
-            },
+	watch: {
+		params: {
+			handler: function (newOptions) {
+				this.getPageDatas(newOptions);
+			},
 
-            deep: true,
-            immediate: true,
-        },
-    },
+			deep: true,
+			immediate: true,
+		},
+	},
 };
 </script>
